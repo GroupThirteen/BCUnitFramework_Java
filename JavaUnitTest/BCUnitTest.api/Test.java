e7b97186122ceb8d24c172894de48ac8edf3619c
Tyler-G99 <65042548+Tyler-G99@users.noreply.github.com>
2022-02-15 10:20:52 -0800

    Create Test.java

---
+++ JavaUnitTest/BCUnitTest.api/Test.java
@@ -0,0 +1,19 @@
+package bcUnitTest.engine;
+
+import java.lang.reflect.Method;
+
+public class TestResult {
+	
+	private final TestStatus testStatus;
+	private final Throwable testStackTraceHolder;
+	private final Method testProcedure;
+	
+	// TODO: Create getters for each, no setters
+	
+	public TestResult(Method procedure, TestStatus status, Throwable information) {
+		this.testStatus = status;
+		this.testStackTraceHolder = information;
+		this.testProcedure = procedure;
+	}
+
+}

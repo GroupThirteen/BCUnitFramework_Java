8c268a737999319ef8cd119977b726a19713d011
Tyler-G99 <65042548+Tyler-G99@users.noreply.github.com>
2022-04-20 12:15:33 -0700

    Update TestEngine.java

--- JavaUnitTest/BCUnitTest.engine/TestEngine.java
+++ JavaUnitTest/BCUnitTest.engine/TestEngine.java
@@ -4,17 +4,42 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.annotation.Annotation;
 
 import bcUnitTest.api.Test;
 
 public class TestEngine {
 	
+	//TODO: add filter() function to extract @Test annotation lines
+	
 	static TestResult run(Method test, Object instance) {
-		return new TestResult(null, TestStatus.SKIPPED, null);
+		try {
+			test.invoke(instance);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new TestResult(test, TestStatus.SKIPPED, null);
 	}
 	
 	public static List<TestResult> run(String testClassName) {
+		ClassLoader loader = ClassLoader.getSystemClassLoader();
 		List<TestResult> results = new ArrayList<>();
+		try {
+			Class<?> type = loader.loadClass(testClassName);
+			Object testClassInstance = type.newInstance();
+			Method[] procedures = type.getMethods();
+			List<Method> tests = new ArrayList<>();
+			for (Method procedure : procedures) {
+				if (procedure.getAnnotation(Test.class) != null) {
+					tests.add(procedure);
+				}
+			}
+			tests.forEach((test) -> {
+				results.add(run(test, testClassInstance));
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return results;
 	}
 	
@@ -25,3 +50,4 @@ public class TestEngine {
 	}
 
 }
+

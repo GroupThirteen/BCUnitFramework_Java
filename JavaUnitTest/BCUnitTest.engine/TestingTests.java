96c7092057f260aed6b5e7cb9078b08036ecf307
Tyler-G99 <65042548+Tyler-G99@users.noreply.github.com>
2022-06-07 13:24:43 -0700

    Update TestingTests.java

--- JavaUnitTest/BCUnitTest.engine/TestingTests.java
+++ JavaUnitTest/BCUnitTest.engine/TestingTests.java
@@ -1,51 +1,68 @@
 package bcUnitTest.engine;
 
 
-import java.util.logging.Logger;
+import bcUnitTest.api.AfterAllTests;
+import bcUnitTest.api.Assert;
+import bcUnitTest.api.BeforeAllTests;
 import bcUnitTest.api.Order;
 import bcUnitTest.api.Test;
 
 public class TestingTests {
-
-	private static final String TEST_CLASS_NAME = "bcUnitTest.engine.TestingTests";
 	
-	private static final Logger INVOCATION_LOGGER = Logger.getLogger(TEST_CLASS_NAME);
-	
-	private static boolean relay(boolean flag) {
-		return flag;
+	@Order(1)
+	@Test
+	public void intTest() {
+		// This test should pass
+		Assert.equals(2, 2);
 	}
 	
-	@Order(value = 2)
+	@Order(2)
 	@Test
-	public void testPassed() {
-		INVOCATION_LOGGER.entering(TEST_CLASS_NAME, "@Test");
-        String msg = "This test should pass";
-        System.out.println(msg);
-        assert relay(true) : msg;
+	public void intTest2() {
+		// This test should fail
+		Assert.equals(3, 2);
 	}
 	
-	
 	@Test
-    public void testFailed() {
-        INVOCATION_LOGGER.entering(TEST_CLASS_NAME, "@Test");
-        String msg = "This test should fail";
-        System.out.println(msg);
-        assert relay(false) : msg;
-    }
+	public void doubleTest2() {
+		// This test should fail
+		Assert.equals(3.2, 4.2);
+	}
+	
+	@BeforeAllTests
+	@Test
+	public void doubleTest() {
+		// This test should pass
+		Assert.equals(3.2, 3.2);
+	}
+	
+	@Order(3)
+	@Test
+	public void stringTest() {
+		// This test should pass
+		Assert.equals("Pass", "Pass");
+	}
+	
+	@Order(4)
+	@Test
+	public void stringTest2() {
+		// This test should fail
+		Assert.equals("Pass", "Fail");
+	}
 	
 	public void testSkipped() {
-        INVOCATION_LOGGER.entering(TEST_CLASS_NAME, "@Test");
-        String msg = "This test should skip";
-        System.out.println(msg);
-        assert relay(false) : msg;
-    }
-	
-	@Order(value = 1)
-	@Test
-    public void testError() {
-        INVOCATION_LOGGER.entering(TEST_CLASS_NAME, "@Test");
-        String msg = "This test should cause an error";
-        System.out.println(msg);
-        throw new RuntimeException(msg);
+        // This test should skip all together
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+

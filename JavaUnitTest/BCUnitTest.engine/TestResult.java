170fec2b57e86bba88ce94e469fb98f907db64e8
Tyler-G99 <65042548+Tyler-G99@users.noreply.github.com>
2022-06-07 13:26:06 -0700

    Update TestResult.java

--- JavaUnitTest/BCUnitTest.engine/TestResult.java
+++ JavaUnitTest/BCUnitTest.engine/TestResult.java
@@ -8,22 +8,24 @@ public class TestResult {
 	private final Throwable testStackTraceHolder;
 	private final Method testProcedure;
 	
-	// TODO: Create getters for each, no setters
-	
+	// Sets the procedure, status and stack trace information for each test to track the results
 	public TestResult(Method procedure, TestStatus status, Throwable information) {
 		this.testStatus = status;
 		this.testStackTraceHolder = information;
 		this.testProcedure = procedure;
 	}
 	
+	// Returns test status
 	public TestStatus getStatus() {
 		return this.testStatus;
 	}
 	
+	// Returns the tests' stack trace
 	public Throwable getTraceHolder() {
 		return this.testStackTraceHolder;
 	}
 	
+	// Returns the name of the test method
 	public Method getProcedure() {
 		return this.testProcedure;
 	}
